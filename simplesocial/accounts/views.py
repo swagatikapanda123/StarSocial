7f8956614904e72d97cf23a4ff8f43f6e679d760
swagatikapanda123 <swagatikapanda164@gmail.com>
2020-03-14 13:20:26 +0530

    initial commit

---
+++ simplesocial/accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.views.generic import CreateView
+from . import forms
+
+# Create your views here.
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    
+    template_name = "accounts/signup.html"
+    
